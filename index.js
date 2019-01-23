29ecbc0a6f362c74561cc02b6494752864fbe06e
April Copes <antdevelopment1@gmail.com>
2019-01-23 18:18:38 -0500

    Saves the path of the url in a variable and trims the path

--- index.js
+++ index.js
@@ -2,10 +2,24 @@
 
 // Dependancy
 var http = require('http');
+var url = require('url');
 
 // The server should respond to all request with a string
 var server = http.createServer(function(req, res) {
+
+    // Get the url and parse it
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get the path from the url
+    var path = parsedUrl.pathname; 
+    var trimmedPath = path.replace(/^\+|\/+$/g, '');
+
+    // Send the response
     res.end('Hello world\n');
+
+    // Log the requested path
+    console.log('Request recieved on this path');
+
 });
 
 // Start the server and have it listen on port 3000
